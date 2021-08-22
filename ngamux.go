e48c45cb303d6ee75383e69caabff8471b023dfc
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-08-22 17:43:50 +0700

    feat(router): register route with method

---
+++ ngamux.go
@@ -0,0 +1,51 @@
+package ngamux
+
+import (
+	"net/http"
+)
+
+type Ngamux struct {
+	router *Router
+}
+
+var _ http.Handler = &Ngamux{}
+
+func NewNgamux() *Ngamux {
+	return &Ngamux{
+		router: newRouter(),
+	}
+}
+func (mux *Ngamux) Get(path string, handler http.HandlerFunc) {
+	mux.router.AddRoute(http.MethodGet, Route{
+		Path:    path,
+		Handler: handler,
+	})
+}
+
+func (mux *Ngamux) Post(path string, handler http.HandlerFunc) {
+	mux.router.AddRoute(http.MethodPost, Route{
+		Path:    path,
+		Handler: handler,
+	})
+}
+
+func (mux *Ngamux) Patch(path string, handler http.HandlerFunc) {
+	mux.router.AddRoute(http.MethodPatch, Route{
+		Path:    path,
+		Handler: handler,
+	})
+}
+
+func (mux *Ngamux) Put(path string, handler http.HandlerFunc) {
+	mux.router.AddRoute(http.MethodPut, Route{
+		Path:    path,
+		Handler: handler,
+	})
+}
+
+func (mux *Ngamux) Delete(path string, handler http.HandlerFunc) {
+	mux.router.AddRoute(http.MethodDelete, Route{
+		Path:    path,
+		Handler: handler,
+	})
+}
---
+++ router.go
@@ -0,0 +1,38 @@
+package ngamux
+
+import (
+	"net/http"
+
+	"github.com/ngamux/gotrie"
+)
+
+type Router struct {
+	routes map[string]*gotrie.Trie
+}
+
+type Route struct {
+	Path    string
+	Handler http.HandlerFunc
+}
+
+func newRouter() *Router {
+	routesMap := map[string]*gotrie.Trie{
+		http.MethodGet:     gotrie.NewTrie(gotrie.Config{Separator: "/"}),
+		http.MethodPost:    gotrie.NewTrie(gotrie.Config{Separator: "/"}),
+		http.MethodPatch:   gotrie.NewTrie(gotrie.Config{Separator: "/"}),
+		http.MethodPut:     gotrie.NewTrie(gotrie.Config{Separator: "/"}),
+		http.MethodDelete:  gotrie.NewTrie(gotrie.Config{Separator: "/"}),
+		http.MethodOptions: gotrie.NewTrie(gotrie.Config{Separator: "/"}),
+		http.MethodConnect: gotrie.NewTrie(gotrie.Config{Separator: "/"}),
+		http.MethodHead:    gotrie.NewTrie(gotrie.Config{Separator: "/"}),
+		http.MethodTrace:   gotrie.NewTrie(gotrie.Config{Separator: "/"}),
+	}
+
+	return &Router{
+		routes: routesMap,
+	}
+}
+
+func (r *Router) AddRoute(method string, route Route) {
+	r.routes[method].Put(route.Path, route)
+}
