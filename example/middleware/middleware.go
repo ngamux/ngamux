af42b7a6d8580364c47d7849769e819a32935fda
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-08-31 10:21:46 +0700

    feat(example): middleware for route

---
+++ example/middleware/middleware.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/ngamux/ngamux"
+)
+
+func MiddlewareHello(handler http.HandlerFunc) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(rw, "hello from middleware")
+		handler(rw, r)
+	}
+}
+
+func main() {
+	mux := ngamux.NewNgamux(ngamux.Config{
+		RemoveTrailingSlash: true,
+	})
+
+	mux.Get("/",
+		MiddlewareHello(
+			MiddlewareHello(
+				MiddlewareHello(
+					MiddlewareHello(
+						func(rw http.ResponseWriter, r *http.Request) {
+							fmt.Fprintln(rw, "hello from handler")
+						},
+					),
+				),
+			),
+		),
+	)
+
+	http.ListenAndServe(":8080", mux)
+}
