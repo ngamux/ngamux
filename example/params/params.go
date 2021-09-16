aa5bc42c29ebc1f798c6500d670c31ec70f6d7f4
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-09-16 07:14:09 +0700

    fix(example): params

--- example/params/params.go
+++ example/params/params.go
@@ -9,21 +9,24 @@ import (
 
 func main() {
 	mux := ngamux.NewNgamux()
-	mux.Get("/", func(rw http.ResponseWriter, r *http.Request) {
+	mux.Get("/", func(rw http.ResponseWriter, r *http.Request) error {
 		fmt.Fprintln(rw, "GET /")
+		return nil
 	})
 
 	users := mux.Group("/users")
-	users.Get("/", func(rw http.ResponseWriter, r *http.Request) {
+	users.Get("/", func(rw http.ResponseWriter, r *http.Request) error {
 		fmt.Fprintln(rw, "GET /users")
+		return nil
 	})
 
-	users.Get("/:id", func(rw http.ResponseWriter, r *http.Request) {
+	users.Get("/:id", func(rw http.ResponseWriter, r *http.Request) error {
 		fmt.Fprintf(rw, "GET /users/:id with id = %s", ngamux.GetParam(r, "id"))
+		return nil
 	})
 
-	users.Get("/:id/:slug", func(rw http.ResponseWriter, r *http.Request) {
-		ngamux.JSON(rw, map[string]string{
+	users.Get("/:id/:slug", func(rw http.ResponseWriter, r *http.Request) error {
+		return ngamux.JSON(rw, map[string]string{
 			"id":   ngamux.GetParam(r, "id"),
 			"slug": ngamux.GetParam(r, "slug"),
 		})
