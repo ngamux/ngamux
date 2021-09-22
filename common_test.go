8cd977a3e9b76ecc4ba93bdbc9b05474990c46ce
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-09-22 23:19:04 +0700

    feat(common): test TestGetParam

--- common_test.go
+++ common_test.go
@@ -1,7 +1,9 @@
 package ngamux
 
 import (
+	"context"
 	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
 )
@@ -24,5 +26,20 @@ func TestWithMiddlewares(t *testing.T) {
 	result = WithMiddlewares(nil)(nil)
 	if result != nil {
 		t.Errorf("TestWithMiddlewares need %v, but got %v", nil, reflect.TypeOf(result))
+	}
+}
+
+func TestGetParam(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	req = req.WithContext(context.WithValue(req.Context(), KeyContextParams, [][]string{{"id", "1"}}))
+	result := GetParam(req, "id")
+
+	if result != "1" {
+		t.Errorf("TestGetParam need %v, but got %v", "1", result)
+	}
+
+	result = GetParam(req, "slug")
+	if result != "" {
+		t.Errorf("TestGetParam need %v, but got %v", "\"\"", result)
 	}
 }
