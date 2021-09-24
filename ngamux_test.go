2d2905d433bee130b5beb1e38837b2d16b787e20
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-09-24 10:47:45 +0700

    feat(mux): test TestUse

--- ngamux_test.go
+++ ngamux_test.go
@@ -1,6 +1,7 @@
 package ngamux
 
 import (
+	"net/http"
 	"reflect"
 	"testing"
 )
@@ -28,5 +29,24 @@ func TestNewNgamux(t *testing.T) {
 
 	if result.regexpParamFinded != expected.regexpParamFinded {
 		t.Errorf("TestNewNgamux need %v, but got %v", expected.regexpParamFinded, result.regexpParamFinded)
+	}
+}
+
+func TestUse(t *testing.T) {
+	mux := NewNgamux()
+	middleware := func(next Handler) Handler {
+		return func(rw http.ResponseWriter, r *http.Request) error {
+			return nil
+		}
+	}
+	mux.Use(middleware)
+	mux.Use(middleware)
+	mux.Use(middleware)
+
+	result := len(mux.middlewares)
+	expected := 3
+
+	if result != expected {
+		t.Errorf("TestUse need %v, but got %v", expected, result)
 	}
 }
