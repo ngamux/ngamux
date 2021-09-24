f9fce16dfeb3cacd693e3d3f12dea30b48bd7257
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-09-24 10:56:57 +0700

    feat(mux): test TestGet

--- ngamux_test.go
+++ ngamux_test.go
@@ -2,7 +2,9 @@ package ngamux
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -50,3 +52,22 @@ func TestUse(t *testing.T) {
 		t.Errorf("TestUse need %v, but got %v", expected, result)
 	}
 }
+
+func TestGet(t *testing.T) {
+	mux := NewNgamux()
+	mux.Get("/", func(rw http.ResponseWriter, r *http.Request) error {
+		return String(rw, "ok")
+	})
+
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	mux.ServeHTTP(rec, req)
+
+	result := strings.ReplaceAll(rec.Body.String(), "\n", "")
+	expected := "ok"
+
+	if result != expected {
+		t.Errorf("TestGet need %v, but got %v", expected, result)
+	}
+}
+
