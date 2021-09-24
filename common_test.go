1d817fbda08de8bbda32744c6a901689d82260e6
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-09-24 08:13:20 +0700

    feat(common): test TestGetJSON

--- common_test.go
+++ common_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -60,5 +61,29 @@ func TestGetQuery(t *testing.T) {
 	result = GetQuery(req, "slug")
 	if result != "" {
 		t.Errorf("TestGetQuery need %v, but got %v", "\"\"", result)
+	}
+}
+
+func TestGetJSON(t *testing.T) {
+	input := strings.NewReader(`{"id": 1}`)
+	req := httptest.NewRequest(http.MethodGet, "/", input)
+
+	var data map[string]interface{}
+	err := GetJSON(req, &data)
+	if err != nil {
+		t.Errorf("TestGetJSON need %v, but got %v", "nil", err)
+	}
+
+	if data["id"] == nil {
+		t.Errorf("TestGetJSON need %v, but got %v", "value", data["id"])
+	}
+
+	id, ok := data["id"].(float64)
+	if !ok {
+		t.Errorf("TestGetJSON need %v, but got %v", "true", ok)
+	}
+
+	if id != 1 {
+		t.Errorf("TestGetJSON need %v, but got %v", 1, id)
 	}
 }
