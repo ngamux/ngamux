034f1d1ed75157877e0a4836cabeab1d595f03a6
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-10-01 08:29:11 +0700

    fix(route): no matched because method replaced

--- route.go
+++ route.go
@@ -17,7 +17,7 @@ type (
 		UrlMatcher *regexp.Regexp
 	}
 
-	routeMap map[string]Route
+	routeMap map[string]map[string]Route
 )
 
 func buildRoute(url string, method string, handler Handler, middlewares ...MiddlewareFunc) Route {
@@ -38,10 +38,15 @@ func (mux *Ngamux) addRoute(route Route) {
 
 	// check if route doesn't have url param
 	if !strings.Contains(route.Path, ":") {
-		mux.routes[route.Path] = route
+		if mux.routes[route.Path] == nil {
+			mux.routes[route.Path] = map[string]Route{}
+		}
+
+		mux.routes[route.Path][route.Method] = route
 		return
 	}
 
+	// building route with url param
 	subMatchs := mux.regexpParamFinded.FindAllStringSubmatch(route.Path, -1)
 	route.Params = [][]string{}
 	for _, val := range subMatchs {
@@ -57,7 +62,29 @@ func (mux *Ngamux) addRoute(route Route) {
 		return
 	}
 
-	mux.routesParam[route.Path] = route
+	if mux.routesParam[route.Path] == nil {
+		mux.routesParam[route.Path] = map[string]Route{}
+	}
+	mux.routesParam[route.Path][route.Method] = route
+}
+
+func buildUrlParams(r *http.Request, route Route, path string) (*http.Request, Route) {
+	if route.UrlMatcher == nil {
+		return r, route
+	}
+
+	foundParams := route.UrlMatcher.FindAllStringSubmatch(path, -1)
+	params := make([][]string, len(route.Params))
+	copy(params, route.Params)
+	for i := range params {
+		params[i] = append(params[i], foundParams[0][i+1])
+	}
+	if len(route.Params) > 0 {
+		route.Params = params
+		ctx := context.WithValue(r.Context(), KeyContextParams, params)
+		r = r.WithContext(ctx)
+	}
+	return r, route
 }
 
 func (mux *Ngamux) getRoute(r *http.Request) (Route, *http.Request) {
@@ -66,38 +93,39 @@ func (mux *Ngamux) getRoute(r *http.Request) (Route, *http.Request) {
 		path = strings.TrimRight(path, "/")
 	}
 
-	foundRoute, ok := mux.routes[path]
+	foundRouteMap, ok := mux.routes[path]
 	if !ok {
 		for url, route := range mux.routesParam {
-			if route.UrlMatcher.MatchString(path) {
-				foundParams := route.UrlMatcher.FindAllStringSubmatch(path, -1)
-				params := make([][]string, len(route.Params))
-				copy(params, route.Params)
-				for i := range params {
-					params[i] = append(params[i], foundParams[0][i+1])
-				}
-				if len(route.Params) > 0 {
-					route.Params = params
-					ctx := context.WithValue(r.Context(), KeyContextParams, params)
-					r = r.WithContext(ctx)
-				}
-				foundRoute = route
+			urlMatcher, err := regexp.Compile("^" + url + "$")
+			if err != nil {
+				break
+			}
+
+			if urlMatcher.MatchString(path) {
+				foundRouteMap = route
 				break
 			}
 
 			if url == path {
-				foundRoute = route
+				foundRouteMap = route
 				break
 			}
 		}
 	}
 
-	if foundRoute.Handler == nil {
+	var foundRoute Route
+	if len(foundRouteMap) <= 0 {
 		r = SetContextValue(r, "error", ErrorNotFound)
 		foundRoute.Handler = mux.config.GlobalErrorHandler
-	} else if foundRoute.Method != "*" && foundRoute.Method != r.Method {
-		r = SetContextValue(r, "error", ErrorMethodNotAllowed)
-		foundRoute.Handler = mux.config.GlobalErrorHandler
+	} else {
+		route, ok := foundRouteMap[r.Method]
+		if !ok {
+			r = SetContextValue(r, "error", ErrorMethodNotAllowed)
+			foundRoute.Handler = mux.config.GlobalErrorHandler
+		} else {
+			r, route = buildUrlParams(r, route, path)
+			foundRoute = route
+		}
 	}
 
 	return foundRoute, r
