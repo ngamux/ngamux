dc0cd94f5996b4d503966291438324c338390ffe
Muhammad Isa WK <msawjk@gmail.com>
2021-10-14 00:21:48 +0700

    revamping new ngamux function to returns interface not directly returns Ngamux struct

--- ngamux.go
+++ ngamux.go
@@ -3,6 +3,8 @@ package ngamux
 import (
 	"errors"
 	"fmt"
+	"github.com/ngamux/ngamux/constants"
+	ngamuxerr "github.com/ngamux/ngamux/errors"
 	"net/http"
 	"regexp"
 )
@@ -14,14 +16,56 @@ const (
 )
 
 type (
+	// Ngamux main components struct defined inside Ngamux
 	Ngamux struct {
-		parent            *Ngamux
-		path              string
-		routes            routeMap
-		routesParam       routeMap
-		config            Config
-		regexpParamFinded *regexp.Regexp
-		middlewares       []MiddlewareFunc
+		parent             *Ngamux
+		path               string
+		routes             routeMap
+		routesParam        routeMap
+		config             Config
+		regexpParamFounded *regexp.Regexp
+		middlewares        []MiddlewareFunc
+	}
+
+	// INgamux representations of *Ngamux functionality
+	INgamux interface {
+		// ServeHTTP overrides from http package
+		ServeHTTP(rw http.ResponseWriter, r *http.Request)
+
+		// Use attaching or appending middleware to your route endpoint as many as you want
+		Use(middlewares ...MiddlewareFunc)
+
+		// Get create route endpoint with http get method operation
+		// It returns nothing
+		Get(url string, handler Handler)
+
+		// Post create route endpoint with http post method operation
+		// It returns nothing
+		Post(url string, handler Handler)
+
+		// Patch create route endpoint with http patch method operation
+		// It returns nothing
+		Patch(url string, handler Handler)
+
+		// Put create route endpoint with http put method operation
+		// It returns nothing
+		Put(url string, handler Handler)
+
+		// Delete create route endpoint with http delete method operation
+		// It returns nothing
+		Delete(url string, handler Handler)
+
+		// All create route endpoint with all http methods operation
+		// It returns nothing
+		All(url string, handler Handler)
+
+		// Group create group of endpoints with specific customizable middleware
+		// It returns *Ngamux
+		Group(url string, middlewares ...MiddlewareFunc) *Ngamux
+
+		ngamux() *Ngamux
+		addRoute(route Route)
+		getRoute(r *http.Request) (Route, *http.Request)
 	}
 )
 
@@ -29,35 +73,38 @@ var (
 	_ http.Handler = &Ngamux{}
 	_ http.Handler = Handler(func(rw http.ResponseWriter, r *http.Request) error { return nil })
 
-	ErrorNotFound         = errors.New("not found")
-	ErrorMethodNotAllowed = errors.New("method not allowed")
-
 	paramsFinder       = regexp.MustCompile("(:[a-zA-Z]+[0-9a-zA-Z]*)")
 	globalErrorHandler = func(rw http.ResponseWriter, r *http.Request) error {
-		err := GetContextValue(r, "error").(error)
-		if errors.Is(err, ErrorNotFound) {
+		err := GetContextValue(r, constants.ContextKeyError).(error)
+
+		if errors.Is(err, ngamuxerr.NotFound) {
 			rw.WriteHeader(http.StatusNotFound)
-		} else if errors.Is(err, ErrorMethodNotAllowed) {
+		} else if errors.Is(err, ngamuxerr.MethodNotAllowed) {
 			rw.WriteHeader(http.StatusMethodNotAllowed)
 		}
 
-		fmt.Fprintln(rw, err)
+		_, _ = fmt.Fprintln(rw, err)
+
 		return nil
 	}
 )
 
-func NewNgamux(configs ...Config) *Ngamux {
+func NewNgamux(configs ...Config) INgamux {
 	config := buildConfig(configs...)
 	routesMap := routeMap{}
 	routesParamMap := routeMap{}
 	router := &Ngamux{
-		routes:            routesMap,
-		routesParam:       routesParamMap,
-		config:            config,
-		regexpParamFinded: paramsFinder,
+		routes:             routesMap,
+		routesParam:        routesParamMap,
+		config:             config,
+		regexpParamFounded: paramsFinder,
 	}
 
 	return router
+}
+
+func (mux *Ngamux) ngamux() *Ngamux {
+	return mux
 }
 
 func (mux *Ngamux) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
@@ -65,7 +112,7 @@ func (mux *Ngamux) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	err := route.Handler(rw, r)
 	if err != nil {
 		rw.WriteHeader(500)
-		rw.Write([]byte(err.Error()))
+		_, _ = rw.Write([]byte(err.Error()))
 	}
 }
 
