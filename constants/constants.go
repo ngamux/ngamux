f445aa80d5bda6510bb5b8750853dfd8a58242e7
Muhammad Isa WK <msawjk@gmail.com>
2021-10-14 00:19:18 +0700

    separate static strings into constants

---
+++ constants/constants.go
@@ -0,0 +1,13 @@
+package constants
+
+const (
+	// NotFoundMessage static string contains `not found`
+	NotFoundMessage = `not found`
+	// MethodNotAllowedMessage static string contains `method not allowed`
+	MethodNotAllowedMessage = `method not allowed`
+)
+
+const (
+	// ContextKeyError get context value with `error` key
+	ContextKeyError = `error`
+)
