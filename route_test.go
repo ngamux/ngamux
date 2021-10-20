fd87c7e87cfb54c33037f581007e7eab3a1efb62
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-10-20 15:56:36 +0700

    refactor(route): test using golang-must assertion lib

--- route_test.go
+++ route_test.go
@@ -5,35 +5,30 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+
+	"github.com/golang-must/must"
 )
 
 func TestBuildRoute(t *testing.T) {
+	must := must.New(t)
 	result := buildRoute("/", http.MethodGet, func(rw http.ResponseWriter, r *http.Request) error { return nil })
 	expected := Route{
 		Path:   "/",
 		Method: http.MethodGet,
 	}
-
-	if result.Path != expected.Path {
-		t.Errorf("TestBuildRoute need %v, but got %v", expected.Path, result.Path)
-	}
-
-	if result.Method != expected.Method {
-		t.Errorf("TestBuildRoute need %v, but got %v", expected.Method, result.Method)
-	}
+	must.Equal(expected.Path, result.Path)
+	must.Equal(expected.Method, result.Method)
 }
 
 func TestAddRoute(t *testing.T) {
+	must := must.New(t)
 	mux := NewNgamux()
 	mux.addRoute(buildRoute("/", http.MethodGet, nil))
 	mux.addRoute(buildRoute("/a", http.MethodGet, nil))
 	mux.addRoute(buildRoute("/b", http.MethodGet, nil))
 	result := len(mux.routes)
 	expected := 3
-
-	if result != expected {
-		t.Errorf("TestAddRoute need %v, but got %v", expected, result)
-	}
+	must.Equal(expected, result)
 
 	mux = NewNgamux()
 	mux.addRoute(buildRoute("/a/:a", http.MethodGet, nil))
@@ -41,13 +36,11 @@ func TestAddRoute(t *testing.T) {
 	mux.addRoute(buildRoute("/c/:c", http.MethodGet, nil))
 	result = len(mux.routesParam)
 	expected = 3
-
-	if result != expected {
-		t.Errorf("TestAddRoute need %v, but got %v", expected, result)
-	}
+	must.Equal(expected, result)
 }
 
 func TestGetRoute(t *testing.T) {
+	must := must.New(t)
 	mux := NewNgamux()
 	mux.Get("/", func(rw http.ResponseWriter, r *http.Request) error {
 		return String(rw, "ok")
@@ -60,10 +53,7 @@ func TestGetRoute(t *testing.T) {
 
 	result := strings.ReplaceAll(rec.Body.String(), "\n", "")
 	expected := "ok"
-
-	if result != expected {
-		t.Errorf("TestGetRoute need %v, but got %v", expected, result)
-	}
+	must.Equal(expected, result)
 
 	mux1 := NewNgamux()
 	mux1.Get("/:a", func(rw http.ResponseWriter, r *http.Request) error {
@@ -77,10 +67,7 @@ func TestGetRoute(t *testing.T) {
 
 	result = strings.ReplaceAll(rec1.Body.String(), "\n", "")
 	expected = "ok"
-
-	if result != expected {
-		t.Errorf("TestGetRoute need %v, but got %v", expected, result)
-	}
+	must.Equal(expected, result)
 
 	req2 := httptest.NewRequest(http.MethodGet, "/123", nil)
 	rec2 := httptest.NewRecorder()
@@ -89,10 +76,7 @@ func TestGetRoute(t *testing.T) {
 
 	result = strings.ReplaceAll(rec2.Body.String(), "\n", "")
 	expected = "not found"
-
-	if result != expected {
-		t.Errorf("TestGetRoute need %v, but got %v", expected, result)
-	}
+	must.Equal(expected, result)
 
 	req3 := httptest.NewRequest(http.MethodPost, "/", nil)
 	rec3 := httptest.NewRecorder()
@@ -101,8 +85,5 @@ func TestGetRoute(t *testing.T) {
 
 	result = strings.ReplaceAll(rec3.Body.String(), "\n", "")
 	expected = "method not allowed"
-
-	if result != expected {
-		t.Errorf("TestGetRoute need %v, but got %v", expected, result)
-	}
+	must.Equal(expected, result)
 }
