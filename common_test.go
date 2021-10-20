a7fc848258ca0f7e02666d7421d73ffbe33975e4
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-10-20 14:27:05 +0700

    refactor(common): test using golang-must assertion lib

--- common_test.go
+++ common_test.go
@@ -4,121 +4,94 @@ import (
 	"context"
 	"net/http"
 	"net/http/httptest"
-	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/golang-must/must"
 )
 
 func TestWithMiddlewares(t *testing.T) {
+	must := must.New(t)
 	result := WithMiddlewares()(func(rw http.ResponseWriter, r *http.Request) error {
 		return nil
 	})
-	if result == nil {
-		t.Errorf("TestWithMiddlewares need %v, but got %v", reflect.TypeOf(result), nil)
-	}
+	must.NotNil(result)
 
 	result = WithMiddlewares(nil)(func(rw http.ResponseWriter, r *http.Request) error {
 		return nil
 	})
-	if result == nil {
-		t.Errorf("TestWithMiddlewares need %v, but got %v", reflect.TypeOf(result), nil)
-	}
+	must.NotNil(result)
 
 	result = WithMiddlewares(nil)(nil)
-	if result != nil {
-		t.Errorf("TestWithMiddlewares need %v, but got %v", nil, reflect.TypeOf(result))
-	}
+	must.Nil(result)
 }
 
 func TestGetParam(t *testing.T) {
+	must := must.New(t)
+
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
 	req = req.WithContext(context.WithValue(req.Context(), KeyContextParams, [][]string{{"id", "1"}}))
 	result := GetParam(req, "id")
-
-	if result != "1" {
-		t.Errorf("TestGetParam need %v, but got %v", "1", result)
-	}
+	must.Equal("1", result)
 
 	result = GetParam(req, "slug")
-	if result != "" {
-		t.Errorf("TestGetParam need %v, but got %v", "\"\"", result)
-	}
+	must.Equal("", result)
 }
 
 func TestGetQuery(t *testing.T) {
+	must := must.New(t)
 	req := httptest.NewRequest(http.MethodGet, "/?id=1", nil)
 	result := GetQuery(req, "id")
-
-	if result != "1" {
-		t.Errorf("TestGetQuery need %v, but got %v", "1", result)
-	}
+	must.Equal("1", result)
 
 	result = GetQuery(req, "slug", "undefined")
-	if result != "undefined" {
-		t.Errorf("TestGetQuery need %v, but got %v", "undefined", result)
-	}
+	must.Equal("undefined", result)
 
 	result = GetQuery(req, "slug")
-	if result != "" {
-		t.Errorf("TestGetQuery need %v, but got %v", "\"\"", result)
-	}
+	must.Equal("", result)
 }
 
 func TestGetJSON(t *testing.T) {
+	must := must.New(t)
 	input := strings.NewReader(`{"id": 1}`)
 	req := httptest.NewRequest(http.MethodGet, "/", input)
 
 	var data map[string]interface{}
 	err := GetJSON(req, &data)
-	if err != nil {
-		t.Errorf("TestGetJSON need %v, but got %v", "nil", err)
-	}
-
-	if data["id"] == nil {
-		t.Errorf("TestGetJSON need %v, but got %v", "value", data["id"])
-	}
+	must.Nil(err)
+	must.NotNil(data["id"])
 
 	id, ok := data["id"].(float64)
-	if !ok {
-		t.Errorf("TestGetJSON need %v, but got %v", "true", ok)
-	}
-
-	if id != 1 {
-		t.Errorf("TestGetJSON need %v, but got %v", 1, id)
-	}
+	must.True(ok)
+	must.Equal(float64(1), id)
 }
 
 func TestSetContextValue(t *testing.T) {
+	must := must.New(t)
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
 	req = SetContextValue(req, "id", 1)
 
 	id := req.Context().Value("id")
-	if id != 1 {
-		t.Errorf("TestSetContextValue need %v, but got %v", 1, id)
-	}
+	must.Equal(1, id)
 
 	slug := req.Context().Value("slug")
-	if id != 1 {
-		t.Errorf("TestSetContextValue need %v, but got %v", nil, slug)
-	}
+	must.Nil(slug)
 }
 
 func TestGetContextValue(t *testing.T) {
+	must := must.New(t)
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
 	req = req.WithContext(context.WithValue(req.Context(), "id", 1))
 
 	id := GetContextValue(req, "id")
-	if id != 1 {
-		t.Errorf("TestGetContextValue need %v, but got %v", 1, id)
-	}
+	must.Equal(1, id)
 
 	slug := GetContextValue(req, "slug")
-	if id != 1 {
-		t.Errorf("TestGetContextValue need %v, but got %v", nil, slug)
-	}
+	must.Nil(slug)
 }
 
 func TestString(t *testing.T) {
+	must := must.New(t)
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
 	rec := httptest.NewRecorder()
 	handler := http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
@@ -128,12 +101,11 @@ func TestString(t *testing.T) {
 
 	result := rec.Body.String()
 	expected := "ok\n"
-	if result != expected {
-		t.Errorf("TestString need %v, but got %v", expected, result)
-	}
+	must.Equal(expected, result)
 }
 
 func TestStringWithStatus(t *testing.T) {
+	must := must.New(t)
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
 	rec := httptest.NewRecorder()
 	handler := http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
@@ -143,18 +115,15 @@ func TestStringWithStatus(t *testing.T) {
 
 	resultBody := rec.Body.String()
 	expectedBody := "ok\n"
-	if resultBody != expectedBody {
-		t.Errorf("TestStringWithStatus need %v, but got %v", expectedBody, resultBody)
-	}
+	must.Equal(expectedBody, resultBody)
 
 	resultStatus := rec.Result().StatusCode
 	expectedStatus := http.StatusOK
-	if resultStatus != expectedStatus {
-		t.Errorf("TestStringWithStatus need %v, but got %v", expectedStatus, resultStatus)
-	}
+	must.Equal(expectedStatus, resultStatus)
 }
 
 func TestJSON(t *testing.T) {
+	must := must.New(t)
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
 	rec := httptest.NewRecorder()
 	handler := http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
@@ -166,12 +135,11 @@ func TestJSON(t *testing.T) {
 
 	resultBody := strings.ReplaceAll(rec.Body.String(), "\n", "")
 	expectedBody := `{"id":1}`
-	if resultBody != expectedBody {
-		t.Errorf("TestJSON need %v, but got %v", expectedBody, resultBody)
-	}
+	must.Equal(expectedBody, resultBody)
 }
 
 func TestJSONWithStatus(t *testing.T) {
+	must := must.New(t)
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
 	rec := httptest.NewRecorder()
 	handler := http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
@@ -183,13 +151,9 @@ func TestJSONWithStatus(t *testing.T) {
 
 	resultBody := strings.ReplaceAll(rec.Body.String(), "\n", "")
 	expectedBody := `{"id":1}`
-	if resultBody != expectedBody {
-		t.Errorf("TestJSONWithStatus need %v, but got %v", expectedBody, resultBody)
-	}
+	must.Equal(expectedBody, resultBody)
 
 	resultStatus := rec.Result().StatusCode
 	expectedStatus := http.StatusOK
-	if resultStatus != expectedStatus {
-		t.Errorf("TestJSONWithStatus need %v, but got %v", expectedStatus, resultStatus)
-	}
+	must.Equal(expectedStatus, resultStatus)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ngamux/ngamux
 
 go 1.17
+
+require github.com/golang-must/must v0.0.0-20211018162553-f536e16d6da8
