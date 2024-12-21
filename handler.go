ac4ea008331dec5ed87b7b06f2c2b3e7e8973c30
hadihammurabi <hadihammurabi@gmail.com>
2024-12-21 09:41:47 +0700

    fix(handler): resolve linter

--- handler.go
+++ handler.go
@@ -5,15 +5,7 @@ import "net/http"
 type (
 	// MiddlewareFunc describe middleware function
 	MiddlewareFunc func(next http.HandlerFunc) http.HandlerFunc
-
-	// Handler describe function handler
-	handler func(rw http.ResponseWriter, r *http.Request)
 )
-
-// ServeHTTP same as original Handler but for built in HTTP HandlerFunc
-func (h handler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	h(rw, r)
-}
 
 func ToHandler(h http.HandlerFunc) http.Handler {
 	return http.Handler(h)
